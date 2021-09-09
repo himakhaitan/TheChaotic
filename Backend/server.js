56dcdb879b3b9fa2d53946a87cd54027ffc19b7f
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2021-09-10 01:42:11 +0530

    DataBase Connection Established

--- Backend/server.js
+++ Backend/server.js
@@ -5,18 +5,36 @@ const helmet = require("./middlewares/helemt");
 const logging = require("./middlewares/logging");
 const http = require("http");
 const app = express();
+
+const session = require("express-session");
+const mongoose = require("mongoose");
+
 app.use(cors);
 app.use(express.json());
 app.use(compression());
 app.use(helmet);
 app.use(logging);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // GENERAL ROUTE
+
+/*
+Method  : GET
+Route   : /
+Access  : Public
+*/
 app.get("/", (req, res) => {
   return res.send({
     server: "The Chaotic",
     status: "online",
-    host: req.headers.host
+    host: req.headers.host,
   });
 });
 
@@ -24,8 +42,20 @@ const PORT = process.env.PORT || 8000;
 const SERVER = http.createServer(app);
 
 // DATABASE CONNECTION
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => {
+    console.log("Datebase Connection Established!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is up and Running at PORT : ${PORT}`);
 });
+
 module.exports = SERVER;
